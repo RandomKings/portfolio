77e6a789afc482e1d8fb043a50d355e84203819c
RandomKings <pranavnathani03@gmail.com>
2024-12-12 18:56:34 +0700

    image fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,73 +1,44 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <mesh>
-      {/* Reduced light intensity and shadow-mapSize */}
-      <hemisphereLight intensity={0.1} groundColor="black" />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={0.8}
-        castShadow
-        shadow-mapSize={512} // Lower shadow resolution
-      />
-      <pointLight intensity={0.8} />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.65 : 0.7}
-        position={isMobile ? [0, -2.5, -2] : [0, -3, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
 };
 
-const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-    setIsMobile(mediaQuery.matches);
-
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
-
+const EarthCanvas = () => {
   return (
     <Canvas
-      frameloop="demand" // Render only on demand
       shadows
-      dpr={[1, 1.5]} // Cap DPR for performance
-      camera={{ position: [15, 3, 5], fov: 30 }} // Adjusted camera settings
-      gl={{ preserveDrawingBuffer: false }} // Disable preserveDrawingBuffer
+      frameloop='demand'
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
+          autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
-      </Suspense>
+        <Earth />
 
-      <Preload all />
+        <Preload all />
+      </Suspense>
     </Canvas>
   );
 };
 
-export default ComputersCanvas;
+export default EarthCanvas;
